7453f19bf10fa80e1cca87e35f03beffc4417fcd
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2023-03-16 16:41:50 +0100

    [wip]: add durable object support

--- message-in-a-bottle/client/src/index.ts
+++ message-in-a-bottle/client/src/index.ts
@@ -4,6 +4,8 @@ import { initialiseGlobe } from "./globe/globe";
 import { getOwnCoordinates } from "./services/coordinatesService";
 import { preloadModel, classifyMessage } from "./services/modelService";
 
+let ws: WebSocket;
+
 /**
  * Attempt to preload the Tensorflow.js model so it's available
  * ahead of time
@@ -12,6 +14,10 @@ preloadModel();
 
 window.addEventListener('DOMContentLoaded', () => {
   doTheCoolStuff();
+});
+
+window.addEventListener("beforeunload", () => {
+  ws.close();
 });
 
 async function doTheCoolStuff() {
@@ -24,12 +30,7 @@ async function doTheCoolStuff() {
     if(ev.key === 'Enter') {
       const { containsToxicSentiments } = await classifyMessage(messageInputEl.value);
 
-      if(containsToxicSentiments) {
-        globe.ringColor(() => "red");
-      } else {
-        globe.ringColor(() => colorInterpolator);
-      }
-
+      ws.send(JSON.stringify({ containsToxicSentiments }))
       messageInputEl.value = "";
     }
   });
@@ -41,10 +42,50 @@ async function doTheCoolStuff() {
   }];
 
   globe = initialiseGlobe(globeContainerEl);
-  globe.ringsData(ownLocationData);
+  // globe.ringsData(ownLocationData);
   globe.pointOfView({
     // always start at own coordinates
     lat: ownLatitude,
     lng: ownLongitude
   });
+
+  let hostname = window.location.host;
+  const wss = document.location.protocol === "http:" ? "ws://" : "wss://";
+  ws = new WebSocket(wss + hostname + "/api/websocket");
+
+  ws.onopen = () => {
+    console.log("WebSocket ready");
+  }
+
+  ws.onmessage = (message) => {
+    const data = JSON.parse(message.data);
+    const joined = data.joined;
+    const quit = data.quit;
+    const messaged = data.messaged;
+
+    // add sessions who joined
+    if(joined) {
+      const { latitude, longitude } = joined;
+      globe.ringsData([...globe.ringsData(), {
+        lat: Number(latitude),
+        lng: Number(longitude),
+        color: colorInterpolator,
+      }]);
+    }
+
+    // remove sessions who quit
+    if(quit) {
+      const { latitude, longitude } = quit;
+      console.log(`Removing ${latitude} lat, ${longitude} long. WebSocket connection was closed.`)
+      const remaining = globe.ringsData().filter(crr => crr.lat != latitude && crr.lng != longitude);
+      globe.ringsData(remaining);
+    }
+
+    // update sessions who sent a message
+    if(messaged) {
+      const { latitude, longitude, containsToxicSentiments } = messaged;
+      const session = globe.ringsData().find(el => el.lat == latitude && el.lng == longitude);
+      session.color = containsToxicSentiments ? () => "red" : colorInterpolator;
+    }
+  }
 }
---
+++ message-in-a-bottle/functions/api/websocket.ts
@@ -0,0 +1,13 @@
+interface Env {
+  GLOBE_DO: DurableObjectNamespace;
+}
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  const { pathname } = new URL(context.request.url);
+  const id = context.env.GLOBE_DO.idFromName(pathname);
+  const stub = context.env.GLOBE_DO.get(id);
+  console.log('carmen')
+  
+  // Pass the request down to the durable object
+  return stub.fetch(context.request, { headers: {upgrade: "websocket"} });
+}
