dcf36953846e974175c980692e95d4a852001563
Carmen Popoviciu <cpopoviciu@cloudflare.com>
2024-04-17 12:14:32 +0100

    wip: glowing icons

--- message-in-a-bottle/client/src/index.ts
+++ message-in-a-bottle/client/src/index.ts
@@ -26,20 +26,26 @@ async function doTheCoolStuff() {
 
       if(containsToxicSentiments) {
         globe.ringColor(() => "red");
+        // globe.htmlElementsData(ownLocationData);
+        // globe.htmlElement(d => createBrokenHeart());
       } else {
         globe.ringColor(() => colorInterpolator);
+        // globe.htmlElementsData(ownLocationData);
+        // globe.htmlElement(d => createGlowingStar());
       }
 
       messageInputEl.value = "";
     }
   });
   
+  // get own coordinates
   const { latitude: ownLatitude, longitude: ownLongitude } = await getOwnCoordinates();
   const ownLocationData = [{
     lat: Number(ownLatitude),
     lng: Number(ownLongitude),
   }];
 
+  // render the globe
   globe = initialiseGlobe(globeContainerEl);
   globe.ringsData(ownLocationData);
   globe.pointOfView({
@@ -47,4 +53,42 @@ async function doTheCoolStuff() {
     lat: ownLatitude,
     lng: ownLongitude
   });
+}
+
+function createGlowingStar() {
+  const glowingStar = document.createElement('div');
+  const star = document.createElement('div');
+  const glow = document.createElement('div');
+
+  star.innerHTML = "🌟";
+  star.style.width = "20px";
+
+  glow.style.position = "absolute";
+  glow.style.top = "50%";
+  glow.style.left = "50%";
+  glow.style["box-shadow"] = "0px 1px 66px 30px #f4a742";
+  
+  glowingStar.appendChild(star);
+  glowingStar.appendChild(glow);
+  
+  return glowingStar;
+}
+
+function createBrokenHeart() {
+  const brokenHeart = document.createElement('div');
+  const heart = document.createElement('div');
+  const glow = document.createElement('div');
+
+  heart.innerHTML = "💔";
+  heart.style.width = "20px";
+
+  glow.style.position = "absolute";
+  glow.style.top = "50%";
+  glow.style.left = "50%";
+  glow.style["box-shadow"] = "0px 1px 66px 30px red";
+  
+  brokenHeart.appendChild(heart);
+  brokenHeart.appendChild(glow);
+  
+  return brokenHeart;
 }
